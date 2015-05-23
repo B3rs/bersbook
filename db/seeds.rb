e75faf38ec123def18438554c423e875e7c2b6cf
Marco Bersani <marco@kunerango.com>
2015-05-23 18:06:49 +0200

    Add database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'h.wolowitz', email: 'bersbook2@bers.so', password: 'password', password_confirmation: 'password')
+User.create(username: 'c.turk', email: 'bersbook3@bers.so', password: 'password', password_confirmation: 'password')
+User.create(username: 'j.dorian', email: 'bersbook4@bers.so', password: 'password', password_confirmation: 'password')
+User.create(username: 'p.cox', email: 'bersbook5@bers.so', password: 'password', password_confirmation: 'password')
+User.create(username: 's.cooper', email: 'bersbook6@bers.so', password: 'password', password_confirmation: 'password')
+User.create(username: 'b.stinson', email: 'bersbook7@bers.so', password: 'password', password_confirmation: 'password')
